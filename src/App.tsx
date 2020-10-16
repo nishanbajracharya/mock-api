d6c0bbc23caf0f1f1ba3fc01fc836e88a48314d0
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-10-16 16:09:38 +0545

    Add firebase basic login

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+import { auth } from './services/firebase';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [user, loading, error] = useAuthState(auth);
+
+  function login() {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  function logout() {
+    return auth.signOut();
+  }
+
   return (
-    <div>Hello World</div>
+    <div>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {!user ? <>
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="email" value={password} onChange={e => setPassword(e.target.value)} />
+
+        <button onClick={login}>Sign in</button>
+      </> : <button onClick={logout}>Log out</button>
+      }
+    </div>
   );
 }
 
