63184f93095b386a3c8e6556435b5a95c0811697
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-10-16 21:14:50 +0545

    Change logo of app

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -2,28 +2,38 @@ import React, { useState } from 'react';
 import Alert from '@material-ui/lab/Alert';
 import { Formik, Form, Field } from 'formik';
 import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
 import { TextField } from 'formik-material-ui';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import LockOpenIcon from '@material-ui/icons/LockOpen';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 import userService from '../../services/user';
 import { loginSchema } from '../../schema/login';
 import { getErrorMessage, ErrorProp } from '../../utils/errorHandler';
 
+import {ReactComponent as Logo} from '../../assets/images/logo.svg';
+
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(16),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+  base: {
+    height: '100vh',
+    boxSizing: 'border-box',
+    paddingTop: theme.spacing(16),
+    background: theme.palette.background.paper,
   },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+  box: {
+    borderRadius: 4,
+    padding: theme.spacing(2),
+    background: theme.palette.background.paper,
+  },
+  paper: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  logo: {
+    margin: theme.spacing(2),
+    fill: theme.palette.secondary.main,
   },
   form: {
     width: '100%',
@@ -40,6 +50,7 @@ const useStyles = makeStyles((theme) => ({
   },
   alert: {
     marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
   }
 }));
 
@@ -60,62 +71,62 @@ function Login() {
   }
 
   return (
-    <Container component="main" maxWidth="xs">
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOpenIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5" className={classes.headerText}>
-          Mock API Admin
-        </Typography>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-          }}
-          validationSchema={loginSchema}
-          onSubmit={(values, { setSubmitting }) => {
-            setError({});
-            login(values.email, values.password)
-              .finally(() => setSubmitting(false));
-          }}
-        >
-          {({ submitForm, isSubmitting }) => (
-            <Form className={classes.form}>
-              <Field
-                component={TextField}
-                name="email"
-                type="email"
-                label="Email"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-              />
-              <Field
-                component={TextField}
-                type="password"
-                label="Password"
-                name="password"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-              />
-              <Button
-                fullWidth
-                className={classes.submit}
-                variant="contained"
-                color="primary"
-                disabled={isSubmitting}
-                onClick={submitForm}
-              >
-                Sign In {isSubmitting && <CircularProgress size={20} className={classes.progress} />}
-              </Button>
-              {error.code && <Alert severity="error" className={classes.alert} onClose={clearError}>{getErrorMessage(error)}</Alert>}
-            </Form>
-          )}
-        </Formik>
-      </div>
-    </Container>
+    <div className={classes.base}>
+      <Container component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <Logo width={48} className={classes.logo}/>
+          <Typography component="h1" variant="h5" className={classes.headerText}>
+            Mock API Admin
+            </Typography>
+          <Formik
+            initialValues={{
+              email: '',
+              password: '',
+            }}
+            validationSchema={loginSchema}
+            onSubmit={(values, { setSubmitting }) => {
+              setError({});
+              login(values.email, values.password)
+                .finally(() => setSubmitting(false));
+            }}
+          >
+            {({ submitForm, isSubmitting }) => (
+              <Form className={classes.form}>
+                <Field
+                  component={TextField}
+                  name="email"
+                  type="email"
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  margin="normal"
+                />
+                <Field
+                  component={TextField}
+                  type="password"
+                  label="Password"
+                  name="password"
+                  variant="outlined"
+                  fullWidth
+                  margin="normal"
+                />
+                <Button
+                  fullWidth
+                  className={classes.submit}
+                  variant="contained"
+                  color="primary"
+                  disabled={isSubmitting}
+                  onClick={submitForm}
+                >
+                  Sign In {isSubmitting && <CircularProgress size={20} className={classes.progress} />}
+                </Button>
+                {error.code && <Alert severity="error" className={classes.alert} onClose={clearError}>{getErrorMessage(error)}</Alert>}
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </Container>
+    </div>
   );
 }
 
