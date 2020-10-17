d61b220507cbe1ba9936fc077f6d4ec31649c716
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-10-17 17:10:08 +0545

    Update collection functions

--- functions/src/collectionRouter.js
+++ functions/src/collectionRouter.js
@@ -1,26 +1,31 @@
 const router = require('express').Router();
-const functions = require('firebase-functions');
-const { getCollectionList } = require('./collectionService');
+const {
+  getCollectionListData,
+  getCollectionData,
+  getCollectionMetadata,
+} = require('./collectionService');
 const { ErrorHandler } = require('./errorHandler');
 
 router.get('/', async (req, res, next) => {
-  return await getCollectionList()
-    .then((data) => {
-      const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
-      const region = functions.config().project.region || 'us-central1';
-
-      return data
-        ? data.list.map((item) => {
-            return {
-              collection: item.title,
-              url: `https://${region}-${projectId}.cloudfunctions.net/api/collections${item.route}`,
-            };
-          }) || []
-        : [];
-    })
+  return await getCollectionListData()
     .then((list) => {
       return res.json({
         title: 'collections',
+        list,
+      });
+    })
+    .catch((err) => {
+      next(new ErrorHandler(500, err));
+    });
+});
+
+router.get('/:collectionID', async (req, res, next) => {
+  const collectionID = req.params.collectionID;
+
+  return await Promise.all([getCollectionData(collectionID), getCollectionMetadata(collectionID)])
+    .then(([list, collection]) => {
+      return res.json({
+        title: collection.title,
         list,
       });
     })
--- functions/src/collectionService.js
+++ functions/src/collectionService.js
@@ -1,3 +1,5 @@
+const functions = require('firebase-functions');
+
 const admin = require('./admin');
 
 function getCollectionList() {
@@ -9,6 +11,49 @@ function getCollectionList() {
   .then(response => response.data());
 }
 
+function getCollection(collectionID) {
+  return admin.firestore().collection(collectionID).get();
+}
+
+function getCollectionListData() {
+  return getCollectionList()
+    .then((data) => {
+      const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+      const region = functions.config().project.region || 'us-central1';
+
+      return data
+        ? data.list.map((item) => {
+            return {
+              collection: item.title,
+              url: `https://${region}-${projectId}.cloudfunctions.net/api/collections${item.route}`,
+            };
+          }) || []
+        : [];
+    })
+}
+
+function getCollectionData(collectionID) {
+  return getCollection(collectionID)
+  .then((snapshot) => {
+    if (snapshot.empty) {
+      return [];
+    }
+
+    return snapshot.docs.map((item) => item.data());
+  })
+}
+
+function getCollectionMetadata(collectionRoute) {
+  return getCollectionList()
+    .then(data => {
+      return data && data.list ? data.list.filter(item => item.route === `/${collectionRoute}`)[0] : []
+    })
+}
+
 module.exports = {
   getCollectionList,
+  getCollection,
+  getCollectionListData,
+  getCollectionData,
+  getCollectionMetadata,
 };
