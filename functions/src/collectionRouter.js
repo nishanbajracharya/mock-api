a413915f7467908696c75e1c447d1c50dd674b2a
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-10-24 22:54:47 +0545

    Add POST request for colletion

--- functions/src/collectionRouter.js
+++ functions/src/collectionRouter.js
@@ -1,8 +1,11 @@
+const yup = require('yup');
 const router = require('express').Router();
+
 const {
   getCollectionListData,
   getCollectionData,
   getCollectionMetadata,
+  createDocument,
 } = require('./collectionService');
 const { ErrorHandler } = require('./errorHandler');
 
@@ -22,13 +25,22 @@ router.get('/', async (req, res, next) => {
 router.get('/:collectionID', async (req, res, next) => {
   const collectionID = req.params.collectionID;
 
-  return await Promise.all([getCollectionData(collectionID), getCollectionMetadata(collectionID)])
+  return await Promise.all([
+    getCollectionData(collectionID),
+    getCollectionMetadata(collectionID),
+  ])
     .then(([list, collection]) => {
       return res.json({
         title: collection.title,
-        list: list.map(listItem => ({
+        list: list.map((listItem) => ({
           id: listItem.id,
-          ...((listItem.fields || []).reduce((acc, current) => ({ ...acc, [current.label || current.displayLabel]: current.value }), {})),
+          ...(listItem.fields || []).reduce(
+            (acc, current) => ({
+              ...acc,
+              [current.label || current.displayLabel]: current.value,
+            }),
+            {}
+          ),
         })),
       });
     })
@@ -36,5 +48,49 @@ router.get('/:collectionID', async (req, res, next) => {
       next(new ErrorHandler(500, err));
     });
 });
+
+const schema = yup.object().shape({
+  displayLabel: yup.string().trim().required('Display Label is required'),
+  fields: yup
+    .array()
+    .of(
+      yup.object().shape({
+        label: yup.string().trim().required(),
+        displayLabel: yup.string().trim(),
+        type: yup.string().trim().required(),
+        value: yup.mixed(),
+      })
+    )
+    .required(),
+});
+
+router.post(
+  '/:collectionID',
+  async (req, res, next) => {
+    const body = req.body;
+
+    try {
+      await schema.validate(body);
+      return next();
+    } catch (error) {
+      return next(new ErrorHandler(400, error));
+    }
+  },
+  async (req, res, next) => {
+    const collectionID = req.params.collectionID;
+
+    const body = req.body;
+
+    return await createDocument(collectionID, body)
+      .then((response) =>
+        res.json({
+          data: response,
+        })
+      )
+      .catch((err) => {
+        next(new ErrorHandler(500, err));
+      });
+  }
+);
 
 module.exports = router;
--- functions/src/collectionService.js
+++ functions/src/collectionService.js
@@ -4,11 +4,11 @@ const admin = require('./admin');
 
 function getCollectionList() {
   return admin
-  .firestore()
-  .collection('meta')
-  .doc('collection')
-  .get()
-  .then(response => response.data());
+    .firestore()
+    .collection('meta')
+    .doc('collection')
+    .get()
+    .then((response) => response.data());
 }
 
 function getCollection(collectionID) {
@@ -16,41 +16,56 @@ function getCollection(collectionID) {
 }
 
 function getCollectionListData() {
-  return getCollectionList()
-    .then((data) => {
-      const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
-      const region = functions.config().project.region || 'us-central1';
+  return getCollectionList().then((data) => {
+    const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
+    const region = functions.config().project.region || 'us-central1';
 
-      return data
-        ? data.list.map((item) => {
-            return {
-              collection: item.title,
-              url: `https://${region}-${projectId}.cloudfunctions.net/api/collections${item.route}`,
-            };
-          }) || []
-        : [];
-    })
+    return data
+      ? data.list.map((item) => {
+          return {
+            collection: item.title,
+            url: `https://${region}-${projectId}.cloudfunctions.net/api/collections${item.route}`,
+          };
+        }) || []
+      : [];
+  });
 }
 
 function getCollectionData(collectionID) {
-  return getCollection(collectionID)
-  .then((snapshot) => {
+  return getCollection(collectionID).then((snapshot) => {
     if (snapshot.empty) {
       return [];
     }
 
     return snapshot.docs.map((item) => item.data());
-  })
+  });
 }
 
 function getCollectionMetadata(collectionRoute) {
-  return getCollectionList()
-    .then(data => {
-      return data && data.list ? data.list.filter(item => item.route === `/${collectionRoute}`)[0] : []
-    })
+  return getCollectionList().then((data) => {
+    return data && data.list
+      ? data.list.filter((item) => item.route === `/${collectionRoute}`)[0]
+      : [];
+  });
+}
+
+function createDocument(collectionID, body) {
+  const collectionRef = admin.firestore().collection(collectionID);
+
+  const documentID = collectionRef.doc().id;
+
+  return collectionRef.doc(documentID).set({
+    fields: [],
+    displayLabel: '',
+    ...body,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+    id: documentID,
+  });
 }
 
 module.exports = {
+  createDocument,
   getCollectionList,
   getCollection,
   getCollectionListData,
