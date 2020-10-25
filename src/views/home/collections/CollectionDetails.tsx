70f79e41dd5e8f9da3f94411df6711a2c6ab5091
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-10-25 08:06:28 +0545

    Add external link to collection

--- src/views/home/collections/CollectionDetails.tsx
+++ src/views/home/collections/CollectionDetails.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Document from '../documents';
+import Link from '@material-ui/core/Link';
 import { useParams } from 'react-router-dom';
 import ListIcon from '@material-ui/icons/List';
+import LaunchIcon from '@material-ui/icons/Launch';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
@@ -46,10 +48,19 @@ function CollectionDetails(props: CollectionDetailComponentProps) {
 
   return <div className={classes.content}>
     <div className={classes.header}>
-      <div>{props.collection?.title}</div>
-      <IconButton edge="end" aria-label="delete" onClick={() => props.onDelete && props.onDelete(props.collection)}>
-        <DeleteIcon />
-      </IconButton>
+      <div>
+        {props.collection?.title}
+      </div>
+      <div>
+        <Link href={`/api/collections/${params.collection}`} target="_blank" rel="noopener">
+          <IconButton>
+            <LaunchIcon />
+          </IconButton>
+        </Link>
+        <IconButton edge="end" aria-label="delete" onClick={() => props.onDelete && props.onDelete(props.collection)}>
+          <DeleteIcon />
+        </IconButton>
+      </div>
     </div>
     <Document collection={props.collection} collections={props.collections} />
   </div>;
